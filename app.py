b058ad7225b234e3bec7b1486de90a742991b74c
sadeleh1989 <sadeleh@studydeets.com>
2024-11-16 03:38:17 +0300

    nnn

--- app.py
+++ app.py
@@ -3,9 +3,9 @@
 Streamlit Chatbot Application with Stylish RTL Design
 """
 
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import os
 import logging
